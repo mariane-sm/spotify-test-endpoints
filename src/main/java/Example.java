96fb2dc99695539d5ee98e2ce90ac75bb0e89d76
Mariane Siqueira Machado <mariane.machado@gmail.com>
2015-05-08 16:04:38 -0300

    Create an example using spotify endpoint

---
+++ src/main/java/Example.java
@@ -0,0 +1,10 @@
+public class Example {
+    public boolean someLibraryMethod() {
+        System.out.println("hello world");
+    	return true; 
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,15 @@
+import static com.jayway.restassured.RestAssured.get;
+import static org.hamcrest.Matchers.equalTo;
+import org.junit.Test;
+import com.jayway.restassured.RestAssured;
+
+public class ExampleTest {
+	
+    @Test
+    public void testingRestAssured() {
+    	
+    	RestAssured.proxy("localhost", 3128);    
+    	get("https://api.spotify.com/v1/artists/3WrFJ7ztbogyGnTHbHJFl2").then().body("name", equalTo("The Beatles"));
+    } 
+}
+
